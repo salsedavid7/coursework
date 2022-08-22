ed4df176654c6b5279d35f3b7cfe75e8d698ed27
salsedavid7 <94432223+salsedavid7@users.noreply.github.com>
2022-08-22 14:18:07 -0700

    Delete CSE107/CSE107_lab3 directory

--- CSE107/CSE107_lab3/histogram.m
+++
@@ -1,6 +0,0 @@
-function histogram (h) 
-x = 1:256; 
-y = h; 
-bar(x,y); 
-xlabel("intensity value"); 
-ylabel("PMF"); 
--- CSE107/CSE107_lab3/script.m
+++
@@ -1,7 +0,0 @@
-%this is script for lab 3 
-dark = imread('Lab_03_image1_dark.tif'); 
-darke = equilize(dark); 
-imwrite(darke, 'dark2_img.png'); 
-%light = imread('Lab_03_image2_light.tif'); 
-%imwrite(light, 'light_img.png'); 
-%lighte = equilize(light); 
